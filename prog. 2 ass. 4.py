5a1a48388538eaf81eddf630c618bdde86704d05
shwetal14 <95999701+shwetal14@users.noreply.github.com>
2022-01-04 15:30:31 +0530

    Add files via upload

---
+++ prog. 2 ass. 4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+nums = [1, 2, 3, 4, 5, 6, 7] 
+
+print("Sample list:",nums)
+
+result = map(lambda x: x + x + x, nums)
+
+print("\n Triple of  list no.s :")
+
+print(list(result))
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
